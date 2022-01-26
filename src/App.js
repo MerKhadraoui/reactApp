f5624db3d6775f98bbfa12b92bfaeec3d09665ec
Khadraoui Meriem <miyakhad@gmail.com>
2022-01-26 11:05:13 +0100

    to do liste

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 function App() {
-  function handelSubmit(e) {
-    e.preventDefault();
+  const [userInput, setUserInput] = useState("");
+  function handleChange(e) {
+    setUserInput(e.target.value.trim());
   }
+
   return (
     <div>
-      <h1>React ap plication </h1>
+      {/* <h1>React ap plication </h1>
       <form onSubmit={handelSubmit}>
         <input type="text" placeholder="Enter your Name" />
       </form>
-      <img src="./images/tree.jpeg" alt="img" />
+      <img src="./images/tree.jpeg" alt="img" /> */}
+      <form>
+        <input type="text" value={userInput} onChange={handleChange} />
+      </form>
     </div>
   );
 }
